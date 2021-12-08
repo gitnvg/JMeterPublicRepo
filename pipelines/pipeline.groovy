eae43acce50ce8afcaf3e18c0511e8d50776a8e8
gitnvg <95258914+gitnvg@users.noreply.github.com>
2021-12-08 14:34:44 -0500

    Added pipeline script via upload

---
+++ pipelines/pipeline.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+    node {
+
+      stage('Initialise') {
+        /* Checkout the scripts */
+        checkout scm: [
+                $class: 'GitSCM',
+                userRemoteConfigs: [
+                        [
+                                url: "https://github.com/gitnvg/JMeterTest.git",
+                                credentialsId: "gitnvg"
+                        ]
+                ],
+                branches: [[name: "main"]]
+        ], poll: false
+      }
+
+      stage('Complete any setup steps') {
+		echo "Complete set-up steps"
+      }
+
+      stage('Execute Performance Tests') {
+        dir("${WORKSPACE}/scripts") {
+			bat 'C:/apache-jmeter-3.2/apache-jmeter-3.2/bin/jmeter.bat -n -t Demo-Google.jmx -l Demo-Google.jtl'
+        }
+      }
+
+      stage('Analyse Results') {
+		echo "Analyse results"
+      }
+    }
